1055ec02c8b764261499969f5382aaa5785a3c90
Mithun Rosinth <44864907+MithunRosinth@users.noreply.github.com>
2022-06-14 22:35:41 +0530

    backup

--- trigger.sh
+++ trigger.sh
@@ -2,12 +2,12 @@
 
 if [[ ! `docker ps -a | awk '{print $NF}' | grep closet_dd` ]]
 then 
-	docker run -p 8000:8000 -d -v $HOME/config/ip_vol:/ip_vol --name closet_dd devsecopscloset/closet_dojo:$BRANCH
+	docker run -p 8000:8000 -d -v $HOME/config/ip_vol:/ip_vol --name closet_dd devsecopscloset/closet_dojo:demo
 fi
 
 if [[ ! `docker ps -a | awk '{print $NF}' | grep closet_octant` ]]
 then
-	docker run -v $HOME/kubeconfig:/kubeconfig -p 7777:7777 -d --name closet_octant devsecopscloset/octant:$BRANCH
+	docker run -v $HOME/kubeconfig:/kubeconfig -p 7777:7777 -d --name closet_octant devsecopscloset/octant:demo
 fi
 
-docker run -v $HOME/config:/root/config -v $HOME/kubeconfig:/root/.kube -e REPO='https://github.com/devsecopscloset/closetserver -b $BRANCH' --name closet_start -d devsecopscloset/closet_start:$BRANCH
+docker run -v $HOME/config:/root/config -v $HOME/kubeconfig:/root/.kube -e REPO='https://github.com/devsecopscloset/closetserver -b $BRANCH' --name closet_start -d devsecopscloset/closet_start:demo
