49744ba935cc3107062ceabc3acd1bd017089436
Mithun Rosinth <44864907+MithunRosinth@users.noreply.github.com>
2022-06-15 15:07:18 +0530

    trigger correction

---
+++ setup.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+set -eu
+
+if [[ -d ~/config ]]
+then
+	if [[ -d ~/config/ip_vol ]]
+	then
+		echo "ip_vol exists"
+	else
+		mkdir ~/config/ip_vol
+	fi
+	if [[ -d ~/config/defect_dojo ]]
+	then
+		echo "defect_dojo exits"
+	else
+		mkdir ~/config/defect_dojo
+	fi
+else
+	mkdir -p ~/config/ip_vol &&
+	mkdir ~/config/defect_dojo
+fi
+
+# Has to be altered if the various pipelines will have various configurations
+if [[ ! -e ~/config/Config.yaml ]]
+then
+	touch ~/config/Config.yaml
+else
+	echo "Pipeline Config exists"
+fi
+if [[ ! -e ~/config/defect_dojo/dd_conf.yaml ]]
+then
+	touch ~/config/defect_dojo/dd_conf.yaml
+else
+	echo "defect-dojo config exists"
+fi
+
+# $kubeconfig is env variable passed from git secrets
+if [[ -d ~/kubeconfig ]]
+then
+	if [[ -e ~/kubeconfig/config ]]
+	then
+		echo "file exists"
+	else
+		touch ~/kubeconfig/config
+	fi
+else
+	mkdir ~/kubeconfig &&
+	touch ~/kubeconfig/config
+fi
+
+gpg --quiet --batch --yes --decrypt --passphrase="$EXKEY" --output ~/trigger/secretex ~/trigger/Secrets.gpg &&
+
+tar -xf ~/trigger/secretex -C ~/trigger/ &&
+
+mv ~/trigger/config ~/kubeconfig/ &&
+mv ~/trigger/Config.yaml ~/config/ &&
+mv ~/trigger/dd_conf.yaml ~/config/defect_dojo/
--- trigger.sh
+++ trigger.sh
@@ -2,12 +2,12 @@
 
 if [[ ! `docker ps -a | awk '{print $NF}' | grep closet_dd` ]]
 then 
-	docker run -p 8000:8000 -d -v $HOME/config/ip_vol:/ip_vol --name closet_dd devsecopscloset/closet_dojo:demo
+	docker run -p 8000:8000 -d -v $HOME/config/ip_vol:/ip_vol --name closet_dd m1v1n/closet_dojo
 fi
 
 if [[ ! `docker ps -a | awk '{print $NF}' | grep closet_octant` ]]
 then
-	docker run -v $HOME/kubeconfig:/kubeconfig -p 7777:7777 -d --name closet_octant devsecopscloset/octant:demo
+	docker run -v $HOME/kubeconfig:/kubeconfig -p 7777:7777 -d --name closet_octant m1v1n/octant
 fi
 
-docker run -v $HOME/config:/root/config -v $HOME/kubeconfig:/root/.kube -e REPO='https://github.com/devsecopscloset/closetserver -b $BRANCH' --name closet_start -d devsecopscloset/closet_start:demo
+docker run -v $HOME/config:/root/config -v $HOME/kubeconfig:/root/.kube --name closet_start -d m1v1n/closet_start
