ae8680f3f49df255739292decad0af69bc140599
Mithun Rosinth <44864907+MithunRosinth@users.noreply.github.com>
2022-06-21 15:07:03 +0530

    container names

--- trigger.sh
+++ trigger.sh
@@ -2,12 +2,12 @@
 
 if [[ ! `docker ps -a | awk '{print $NF}' | grep closet_dd` ]]
 then 
-	docker run -p 8000:8000 -d -v $HOME/config/ip_vol:/ip_vol --name closet_dd devsecopscloset/closet_dojo
+	docker run -p 8000:8000 -d -v $HOME/config/ip_vol:/ip_vol --name closet_dd devsecopscloset/closet_dojo:demo
 fi
 
 if [[ ! `docker ps -a | awk '{print $NF}' | grep closet_octant` ]]
 then
-	docker run -v $HOME/kubeconfig:/kubeconfig -p 7777:7777 -d --name closet_octant devsecopscloset/octant
+	docker run -v $HOME/kubeconfig:/kubeconfig -p 7777:7777 -d --name closet_octant devsecopscloset/octant:demo
 fi
 
 if [[ ! `docker ps -a | awk '{print $NF}' | grep closet_start` ]]
